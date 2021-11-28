85b010af08f8bca0350fd293bc4ffb63487add2d
Rohan Nath <rohannath1510@gmail.com>
2021-11-28 10:13:58 +0530

    1st commit only home page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './mycomponents/Header';
+import HomePage from './mycomponents/HomePage';
+import { Footer } from './mycomponents/Footer';
+
+function App() {
+    return (
+        <>
+            <Header title="MAR Simplified"/>
+            <HomePage />
+            <Footer/>
+        </>
+    )
+}
+
+export default App;
---
+++ src/mycomponents/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+
+    return (
+        <footer className="flex-shrink-0 footer bg-dark text-light  py-2" >
+            <p className= "text-center">&copy; 2021 All rights reserved<br/> Made with  <b>&hearts;</b> by Priyabrata | Pulak | Rohan</p>
+
+        </footer>
+    )
+}
